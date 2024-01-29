251b3592c9598c89454d462fb2aacf3456fc01b9
pbrazzle <prb50@pitt.edu>
2024-01-28 19:10:36 -0500

    Fixed some Config parsing

--- src/main/simpleMain.cpp
+++ src/main/simpleMain.cpp
@@ -23,31 +23,39 @@ ncycle_t getMaxCycles(char* arg) {
 }
 
 void setAddressScheme(const NVM::Simulation::Config& conf) {
+    ConfigParser parser;
     ComponentCounts counts;
 
-    counts.rows = conf.get<int>("DBCS");
-    counts.cols = conf.get<int>("DOMAINS");
-    counts.ranks = conf.get<int>("RANKS");
-    counts.banks = conf.get<int>("BANKS");
-    counts.channels = conf.get<int>("CHANNELS");
+    parser.registerValue<unsigned int>("DBCS", 64, &counts.rows);
+    parser.registerValue<unsigned int>("DOMAINS", 512, &counts.cols);
+    parser.registerValue<unsigned int>("RANKS", 1, &counts.ranks);
+    parser.registerValue<unsigned int>("BANKS", 1, &counts.banks);
+    parser.registerValue<unsigned int>("CHANNELS", 1, &counts.channels);
 
-    setScheme(conf.get<std::string>("AddressMappingScheme"), counts);
+    std::string addressScheme;
+    parser.registerValue<std::string>("AddressMappingScheme", "RK:BK:CH:R:C",
+                                      &addressScheme);
+
+    parser.parse(conf);
+
+    setScheme(addressScheme, counts);
 }
 
 void setLogLevel(const NVM::Simulation::Config& conf) {
-    try {
-        auto logLevel = conf.get<std::string>("LogLevel");
+    ConfigParser parser;
+    std::string logLevel;
+    parser.registerValue<std::string>("LogLevel", "STAT", &logLevel);
+    parser.parse(conf);
 
-        if (logLevel == "DEBUG") {
-            log().setGlobalLevel(LogLevel::DEBUG);
-        } else if (logLevel == "EVENT") {
-            log().setGlobalLevel(LogLevel::EVENT);
-        } else if (logLevel == "STAT") {
-            log().setGlobalLevel(LogLevel::STAT);
-        } else if (logLevel == "ERROR") {
-            log().setGlobalLevel(LogLevel::ERROR);
-        }
-    } catch (...) {
+    if (logLevel == "DEBUG") {
+        log().setGlobalLevel(LogLevel::DEBUG);
+    } else if (logLevel == "EVENT") {
+        log().setGlobalLevel(LogLevel::EVENT);
+    } else if (logLevel == "STAT") {
+        log().setGlobalLevel(LogLevel::STAT);
+    } else if (logLevel == "ERROR") {
+        log().setGlobalLevel(LogLevel::ERROR);
+    } else {
         log().setGlobalLevel(LogLevel::STAT);
     }
 }
