1f54a9c47711b0dcd365b1352bdc6947a4548a94
pbrazzle <prb50@pitt.edu>
2024-02-19 12:47:30 -0500

    Stubbed Bank test

---
+++ test/MemoryTypes/Component/Bank.test.cpp
@@ -0,0 +1,7 @@
+#include "MemoryTypes/Component/Bank/Bank.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace NVM::ComponentType;
+
+TEST_CASE("Works properly", "[Component], [Bank]") {}
